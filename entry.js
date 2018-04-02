92a07e9a1aab29f24f3b1dc49c816580c4543ff0
Edan Lewis <edan@lewisarts.com>
2018-04-01 23:20:58 -0700

    Hooks up volume/detune

--- entry.js
+++ entry.js
@@ -2,9 +2,22 @@ var Tone = require("Tone");
 
 function start() {
   var synth = new Tone.PolySynth(6, Tone.Synth).toMaster();
+  const ranges = document.querySelectorAll(".player__slider");
+  const detune = Array.from(ranges).filter(range => range.name === "detune");
+  const volume = Array.from(ranges).filter(range => range.name === "volume");
+  console.log(detune[0]);
+  detune[0].addEventListener("change", function(e) {
+    detuneChange(e.target.value, synth);
+  });
+  volume[0].addEventListener("change", function(e) {
+    console.log(e.target.value);
+    volumeChange(e.target.value, synth);
+  });
+  volume[0].addEventListener("mousemove", volumeChange(volume, synth));
   document.addEventListener("keydown", function(e) {
     let key = document.querySelector(`li[data-key="${e.keyCode}"]`);
     if (!key) return;
+    if (Array.from(key.classList).includes("active")) return;
     key.classList.add("active");
     keyDown(key, synth, e);
   });
@@ -14,22 +27,20 @@ function start() {
     key.classList.remove("active");
     release(key, synth, e);
   });
-
-  //attach a listener to all of the buttons
-  document.querySelectorAll("li").forEach(function(button) {
-    button.addEventListener("mouseover", function(e) {
-      //play the note on mouse down
-      attack(synth, e);
-    });
-    button.addEventListener("mouseout", function(e) {
-      release(synth);
-    });
-    button.addEventListener("mouseup", function(e) {
-      //release on mouseup
-      release(synth);
-    });
-  });
 }
+
+function detuneChange(range, synth) {
+  synth.set("detune", range);
+}
+
+function volumeChange(range, synth) {
+  if(typeof range === 'object') {
+    synth.set("volume", range[0].value);
+  } else {
+    synth.set("volume", range);
+  }
+}
+
 function attack(synth, e) {
   synth.triggerAttack(e.target.textContent);
 }
