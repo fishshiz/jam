57701caa426b7b517e22dc2e3de0c308886a603f
Edan Lewis <edan@lewisarts.com>
2018-04-29 17:10:22 -0700

    fixes metronome wrap around

--- app/components/timeline.jsx
+++ app/components/timeline.jsx
@@ -12,7 +12,15 @@ export default class Timeline extends React.Component {
       paused: true,
       bpm: 60
     };
+
     Tone.Transport.bpm.value = 60;
+    // Tone.Transport.loop = true;
+    Tone.Transport.loopEnd = "4m";
+    let that = this;
+    Tone.Transport.scheduleRepeat(function(time) {
+      that.start(time);
+      that.loop();
+    }, "4n");
 
     //start the Transport for the events to start
     // Tone.Transport.start();
@@ -22,18 +30,17 @@ export default class Timeline extends React.Component {
     this.stop = this.stop.bind(this);
     this.submitBPM = this.submitBPM.bind(this);
     this.updateField = this.updateField.bind(this);
+    this.loop = this.loop.bind(this);
   }
   togglePlay() {
     if (this.state.paused) {
       this.state.paused = false;
-      let that = this;
-      Tone.Transport.scheduleRepeat(function(time) {
-          that.start(time);
-        }, "4n");
-        Tone.Transport.start();
-      } else {
+      
+      
+      Tone.Transport.start();
+    } else {
       this.state.paused = true;
-              Tone.Transport.stop();
+      Tone.Transport.stop();
 
       this.stop();
     }
@@ -53,29 +60,35 @@ export default class Timeline extends React.Component {
     this.state.player.stop();
   }
 
+  loop() {
+    let progress = (Tone.Transport.seconds % Tone.Transport.loopEnd) / Tone.Transport.loopEnd;
+    progress = progress - Math.floor(progress);
+    if (progress > 1) {
+      document.getElementById("bar").setAttribute("style", "left: 0");
+    } else {
+      let num = (progress * 100).toFixed(2);
+      document.getElementById("bar").setAttribute("style", `left: ${num}%`);
+    }
+  }
+
   submitBPM() {
     let bpm = this.state.bpm;
     Tone.Transport.bpm.value = bpm;
-    console.log(this.state.bpm);
   }
 
   render() {
-    return (
-      <div className="timeline__container">
+    return <div className="timeline__container">
         <button onClick={this.togglePlay}>Play</button>
         <form>
-          <input
-            id="bpm"
-            type="number"
-            value={this.state.bpm}
-            onChange={this.updateField}
-          />
+          <input id="bpm" type="number" value={this.state.bpm} onChange={this.updateField} />
           <button type="button" onClick={this.submitBPM}>
             Enter BPM
           </button>
         </form>
-        <div className="timeline" />
-      </div>
-    );
+
+        <div className="timeline">
+          <div id="bar"> </div>
+        </div>
+      </div>;
   }
 }
